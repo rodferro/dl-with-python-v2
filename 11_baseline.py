0d79cd8e9daa5e45f5d6e46a3bc7d6e4284c88dd
rodferro <rod_ferro@msn.com>
2022-09-05 16:13:20 -0300

    add script

---
+++ 11_baseline.py
@@ -0,0 +1,37 @@
+# Regression Example With Boston Dataset: Baseline
+import pandas as pd
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from scikeras.wrappers import KerasRegressor
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+
+# load datase
+dataframe = pd.read_csv("./data/housing.csv", delim_whitespace=True, header=None)
+dataset = dataframe.values
+
+# split into input (X) and output (Y) variables
+X = dataset[:, :13]
+Y = dataset[:, 13]
+
+# define base model
+def baseline_model():
+    # create model
+    model = Sequential()
+    model.add(
+        Dense(13, input_shape=(13,), kernel_initializer="normal", activation="relu")
+    )
+    model.add(Dense(1, kernel_initializer="normal"))
+
+    # compile model
+    model.compile(loss="mean_squared_error", optimizer="adam")
+    return model
+
+
+# evaluate model
+estimator = KerasRegressor(model=baseline_model, epochs=100, batch_size=5, verbose=0)
+kfold = KFold(n_splits=10)
+results = cross_val_score(estimator, X, Y, cv=kfold, scoring="neg_mean_squared_error")
+print("Results: %.2f (%.2f) MSE" % (results.mean(), results.std()))
